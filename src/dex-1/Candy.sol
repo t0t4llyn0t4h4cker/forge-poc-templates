4e5f9386abff0ac72ab404bcd4f5bc9fd81717ed
NeedMoreCoffee <t0t4llyn0t4h4cker@gmail.com>
2023-05-09 16:46:47 -0500

    upload?

--- src/dex-1/Candy.sol
+++ src/dex-1/Candy.sol
@@ -1,4 +1,15 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.13;
 
+import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
 import "@uniswap-v2-periphery/interfaces/IUniswapV2Router02.sol";
+
+interface IWETH is IERC20 {
+    function deposit() external payable;
+    function transfer(address to, uint256 value) external returns (bool);
+    function withdraw(uint256) external;
+}
